ee88ad4103e69e6da0b004c78da00dff8b4892df
Oliver Tran <99072420+Tranthan22@users.noreply.github.com>
2022-09-28 22:54:11 +0700

    Add files via upload

---
+++ processData/connect.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+#include<ctime>
+#include<cstdlib>
+#include<string.h>
+#include<iomanip>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+void filterData(int argc , char *argv[]);
+void searchData();
+void statisticsData();
+void sortData();
+void a();
---
+++ processData/main.cpp
@@ -0,0 +1,9 @@
+#include "connect.h"
+
+int main(int argc , char *argv[]){
+    
+    filterData(argc , argv);
+    searchData();
+    statisticsData();
+    sortData();
+}
